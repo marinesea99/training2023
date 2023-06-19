76eb045de8475db1105f4e1f132f211c4cb37809
marinesea99 <abc0972521139@gmail.com>
2023-06-19 16:22:24 +0800

    20230619

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -8,8 +8,25 @@ public class Clock : MonoBehaviour {
 	[SerializeField]
 	Transform hoursPivot, minutesPivot, secondsPivot;
 
+	const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
 	void Awake () {
-		Debug.Log(DateTime.Now);
-		hoursPivot.localRotation = Quaternion.Euler(0, 0, -30);
+		DateTime time = DateTime.Now;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, hoursToDegrees * time.Hour, 0f);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, minutesToDegrees * time.Minute, 0f);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, secondsToDegrees * time.Second, 0f);
+	}
+
+	void Update () {
+		TimeSpan time = DateTime.Now.TimeOfDay;
+		hoursPivot.localRotation =
+			Quaternion.Euler(0f, hoursToDegrees * (float)time.TotalHours, 0f);
+		minutesPivot.localRotation =
+			Quaternion.Euler(0f, minutesToDegrees * (float)time.TotalMinutes, 0f);
+		secondsPivot.localRotation =
+			Quaternion.Euler(0f, secondsToDegrees * (float)time.TotalSeconds, 0f);
 	}
 }
