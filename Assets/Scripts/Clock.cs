6bbe35872ab7dbf18f7691847b65edb78edcd4ae
marinesea99 <abc0972521139@gmail.com>
2023-06-19 15:54:33 +0800

    day1

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+	[SerializeField]
+	Transform hoursPivot, minutesPivot, secondsPivot;
+
+	void Awake () {
+		Debug.Log(DateTime.Now);
+		hoursPivot.localRotation = Quaternion.Euler(0, 0, -30);
+	}
+}
